4a34500d59379a9de5ad942b5e10cbc5a2cc2ccb
Borys Senyk <borys.senyk@gmail.com>
2015-11-25 01:40:12 +0100

    Add back button to detail page

--- app/views/topic/topic.js
+++ app/views/topic/topic.js
@@ -1,8 +1,15 @@
+var frameModule = require("ui/frame");
+
 var topicView = {};
 
 topicView.pageNavigatedTo = function(args) {
-    var page = args.object;
-    page.bindingContext = page.navigationContext;
+  var page = args.object;
+  page.bindingContext = page.navigationContext;
+}
+
+topicView.buttonBackTap = function(args) {
+  var topmost = frameModule.topmost();
+  topmost.goBack();
 }
 
 module.exports = topicView;
