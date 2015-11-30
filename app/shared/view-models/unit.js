8f45829bdfa4ac8b07ec7ac3a34d5954b2b77209
Borys Senyk <borys.senyk@gmail.com>
2015-11-30 10:30:38 +0100

    replace single card with an array of cards

--- app/shared/view-models/unit.js
+++ app/shared/view-models/unit.js
@@ -1,11 +1,21 @@
 var observable = require("data/observable");
+var observableArrayModule = require("data/observable-array");
+
+var localArray = new observableArrayModule.ObservableArray();
+
+localArray.push([
+  {htmlString:"<span>I'm some html</span>"}]);
 
 var unitModel = new observable.Observable();
 
-Object.defineProperty(unitModel, "htmlString", {
+Object.defineProperty(unitModel, "cardItems", {
     get: function () {
-        return "<span>I'm some html</span>";
+        return localArray;
     },
+    getItem: function(index) {
+      return localArray.getItem(index);
+    },
+    enumerable: true,
     configurable: true
 })
 
